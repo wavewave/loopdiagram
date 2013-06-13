84453880f012cf39484f4a693a50c39a5c53cad6
Ian-Woo Kim <ianwookim@gmail.com>
2013-06-12 23:15:20 -0400

    implement vertex in-out matching

--- loop.hs
+++ loop.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
 import Control.Applicative ((<$>))
+import Data.Foldable (msum)
+import Data.Maybe (fromJust)
 
 data Label = S | Sc | D | Dc 
            deriving (Show,Eq,Ord)
@@ -8,7 +10,7 @@ data Label = S | Sc | D | Dc
 data Dir = I | O 
          deriving (Show,Eq,Ord)
 
-data Ptl = Ptl Label Dir 
+data Ptl = Ptl { ptlLabel :: Label , ptlDir :: Dir }
          deriving (Show,Eq,Ord)
 
 data Partition = I2 | I3 | I4 
@@ -22,14 +24,17 @@ deriving instance (Show a, Show b) => Show (Comb a b)
 
 deriving instance (Ord a, Ord b) => Ord (Comb a b) 
 
+data VLabel = V1 | V2 | V3 | V4 
+            deriving (Show,Eq,Ord,Enum)
+
 
 data FDir = FDir Dir Bool 
             deriving (Show,Eq,Ord) 
 
-data FLine = FL (Int,Int) FDir 
+data FLine = FL (VLabel,VLabel) FDir 
            deriving (Show,Eq,Ord)
 
-data SLine = SL (Int,Int) Dir
+data SLine = SL (VLabel,VLabel) Dir
            deriving (Show,Eq,Ord)
 
 data Blob comb = Blob Ptl Ptl Ptl Ptl comb
@@ -38,16 +43,15 @@ data Blob comb = Blob Ptl Ptl Ptl Ptl comb
 isValidComb :: Comb Partition Partition -> Bool  
 isValidComb (Comb p1 p2) = p1 /= p2 
 
+makePair :: Partition -> (VLabel,VLabel)
+makePair I2 = (V1,V2)
+makePair I3 = (V1,V3)
+makePair I4 = (V1,V4) 
 
-makePair :: Partition -> (Int,Int)
-makePair I2 = (1,2)
-makePair I3 = (1,3)
-makePair I4 = (1,4) 
-
-makePairComplement :: Partition -> (Int,Int) 
-makePairComplement I2 = (3,4)
-makePairComplement I3 = (2,4)
-makePairComplement I4 = (2,3)
+makePairComplement :: Partition -> (VLabel,VLabel) 
+makePairComplement I2 = (V3,V4)
+makePairComplement I3 = (V2,V4)
+makePairComplement I4 = (V2,V3)
 
 
 makeFPair :: (FDir,FDir) -> Partition -> (FLine,FLine)   
@@ -101,6 +105,49 @@ makeAllBlob :: Blob () -> [Blob (Comb (FLine,FLine) (SLine,SLine))]
 makeAllBlob (Blob pt1 pt2 pt3 pt4 ()) = 
   Blob pt1 pt2 pt3 pt4 <$>  [x | c <- allcomb, x <- makeAllCombFromPartition c ]
 
+scalarVertexDir :: SLine -> VLabel -> Maybe Dir 
+scalarVertexDir (SL (v1,v2) d) v 
+  | v == v1 && d == I = Just O  
+  | v == v2 && d == I = Just I 
+  | v == v1 && d == O = Just I
+  | v == v2 && d == O = Just O 
+  | otherwise = Nothing 
+
+fermionVertexDir :: FLine -> VLabel -> Maybe Dir 
+fermionVertexDir (FL (v1,v2) (FDir d havemass)) v 
+  | v == v1 && d == I && havemass     = Just O  
+  | v == v1 && d == I && not havemass = Just O  
+  | v == v2 && d == I && havemass     = Just O 
+  | v == v2 && d == I && not havemass = Just I
+  --  
+  | v == v1 && d == O && havemass     = Just I
+  | v == v1 && d == O && not havemass = Just I
+  | v == v2 && d == O && havemass     = Just I 
+  | v == v2 && d == O && not havemass = Just O 
+  | otherwise = Nothing 
+
+
+
+vertexDirection :: Blob (Comb (FLine,FLine) (SLine,SLine)) -> VLabel -> (Dir,Dir,Dir)
+vertexDirection (Blob p1 p2 p3 p4 (Comb (f1,f2) (s1,s2))) v = 
+  let d1 = case v of 
+             V1 -> ptlDir p1
+             V2 -> ptlDir p2
+             V3 -> ptlDir p3 
+             V4 -> ptlDir p4 
+      d2 = (fromJust . msum . map (flip fermionVertexDir v)) [f1,f2]
+      d3 = (fromJust . msum . map (flip scalarVertexDir v)) [s1,s2] 
+  in (d1,d2,d3)
+
+matchDirection :: [(Dir,Dir,Dir)] -> Blob (Comb (FLine,FLine) (SLine,SLine)) -> Bool 
+matchDirection dirs blob = all (\x -> (vertexDirection blob x) `elem` dirs) [V1,V2,V3,V4]
+
+
+{-
+vertexDirection (Blob p1 p2 p3 p4 (Comb (f1,f2) (s1,s2))) V2 = ptlDir p2
+vertexDirection (Blob p1 p2 p3 p4 (Comb (f1,f2) (s1,s2))) V3 = ptlDir p3
+vertexDirection (Blob p1 p2 p3 p4 (Comb (f1,f2) (s1,s2))) V4 = ptlDir p4
+-}
 
 main = do 
   putStrLn "loop" 
@@ -108,4 +155,7 @@ main = do
   -- let Blob _ _ _ _ cmb = test 
   -- print $ isValidComb cmb
   -- print ( allcomb' == allcomb)
-  print $ makeAllBlob test 
+  let allblobs = makeAllBlob test
+      matchedblobs =  filter (matchDirection [(I,I,I),(O,O,O)]) allblobs
+
+  mapM_ print matchedblobs
